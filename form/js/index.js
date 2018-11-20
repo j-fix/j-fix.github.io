bf1db31074162554b8154c18d39377334a535df3
Egor Rumyantsev <RumyantsevGT@ya.ru>
2018-11-20 22:51:14 +0300

    Update index.js

--- form/js/index.js
+++ form/js/index.js
@@ -19,7 +19,7 @@ function sendAjaxForm(result_form, ajax_form, url) {
         	$('#result_form').html('Имя: '+result.name+'<br>Телефон: '+result.phonenumber);
     	},
     	error: function(response) { // Данные не отправлены
-            $('#result_form').html('Ошибка. Данные не отправлены.');
+            $('#result_form').html('<span style="color:red;font-weight:900;">Ошибка. Данные не отправлены.</span>');
     	}
  	});
 }
